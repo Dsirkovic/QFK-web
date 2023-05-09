31ac2a61a29126e21d6c0caca8ef1a1af2b2e38e
Dsirkovic <109756190+Dsirkovic@users.noreply.github.com>
2023-05-10 00:32:01 +0200

    Create MenuList.js

---
+++ src/helpers/MenuList.js
@@ -0,0 +1,169 @@
+import Bruschetta from "../assets/bruschetta.jpg";
+import DeluxePizza from "../assets/pizza.jpg";
+import Tofu from "../assets/tofu.jpg";
+import Gnocchi from "../assets/gnocchi.jpg";
+import Pot from "../assets/pot.jpg";
+import Porkloin from "../assets/porkchop.jpg";
+
+export const Menulist = [
+  {
+    name: "Bruschetta",
+    image: Bruschetta,
+    CookTime: 20,
+    recipe: <p>
+      Here’s one of the QFK staples for today, the best thing that can happen to a piece of bread, very simple and completely idiot proof - bruschetta with cream cheese and salsa! Who doesn’t love something this easy to eat and super flavourful?<br></br><br></br>
+
+      INGREDIENTS:<br></br><br></br>
+      Sliced sourdough bread(any kind works, but something with a wider surface, like a diagonally sliced baguette, works best)<br></br><br></br>
+
+Cream cheese(You can also use any kind of cheese that’s easily spreadable on the bread)<br></br><br></br>
+
+For the salsa:
+1-2 medium tomatoes
+1 medium cucumber
+1 small onion
+Olive oil
+Acetto balsamicco
+A couple of sprigs of parsley
+The juice of one lemon
+Salt, pepper and other spices to make your salsa the way you want it(I used cayenne pepper, ground cumin, sweet red paprika, one dried chilli, garlic powder and a bit of brown sugar)<br></br><br></br>
+
+A whole lot of slicing and dicing in this recipe, not a whole lot of other stuff to do. Coarsely dice your tomatoes and cucumber and finely dice your onion, then dump all of it into a bowl and mix in all the other salsa ingredients. Ideally leave it in the fridge for an hour or two before serving so the flavors can get to know eachother.<br></br><br></br>
+
+For the bread, simply slice diagonally to get a bigger surface you can put your toppings on, then drizzle with a bit of olive oil and press down into a hot pan(a griddle pan would work best). The olive oil is the most important bit here because it will help your bread get a golden brown color, instead of just burning up and drying out.<br></br><br></br>
+
+After your bread is done, simply spread your cream cheese generously and top with your salsa, and you have a fancy dinner snack, or a light and healthy brunch!
+    </p>,
+  },
+  
+  {
+    name: "Deluxe Pizza",
+    image: DeluxePizza,
+    CookTime: 60,
+    recipe: <p>
+      Pizza-Made Easy<br></br><br></br>
+
+Title says it all,sure you can just order one,but trust me ,it wont taste as good. Besides,you like cooking dont you? Full disclosure,you show this to a person from Italy and they would probably shoot you.In this case,just remind them they lost World War II.Lets get started:<br></br><br></br>
+
+Ingredients:<br></br><br></br>
+-premade pizza dough (trust me,youll enjoy it far more than having flour everywhere in your kitchen,just make sure its the size of your baking tray)
+-300 g canned crushed tomatos
+-4 cloves of garlic
+-2 red onions
+-50 g of ham
+-50 g of your preffered cheese(we used emmentaler)
+-1 ball of mozzarela cheese
+-1 red bell pepper<br></br><br></br>
+
+Instructions:<br></br><br></br>
+First,we have to make the sauce.Finely dice one of your onions and all of your garlic.Place a pot over medium heat and pour some olive oil in.Throw in your onion and garlic and stir for around a minute.Next ,add your crushed tomatos and season with salt,pepper,two teaspoons of sugar,oregano and basil.Mash the tomatos with a woden spoon,stir,switch to low heat and let it simmer for around half an hour or until a thin crust starts to form on top.While thats happening,chop up and prepare your preffered toppings,we used onions and peppers but you can use anything you want,who's gonna judge you? Once the sauce is done,preheat your oven to 180C and roll out your dough on your baking tray.Pour out the sauce on it,spread it around and then place your cheese,ham and toppings on it(dont place the mozzarella on it yet).Place the tray in your oven and bake for 10 min.Take out the tray and place thin slices of mozzarella on it.Put it back in the oven and bake for another 10 min.Now all you have to do is control yourself for another 10 min for it to cool down.Have fun!
+    </p>,
+  },
+  {
+    name: "Tofu Stir Fry",
+    image: Tofu,
+    CookTime: 40,
+    recipe: <p>
+      Another veggie dish for today folks! This one’s a strong contender on the “throw sh*t into a pan until it’s delicious” team, and it’s fully vegetarian – simple tofu and veggies stir fry.<br></br><br></br>
+
+Ingredients:<br></br><br></br>
+1 tablespoon olive oil
+3-4 sweet red bell peppers
+1 large onion
+2 medium-large zucchini
+100g tofu
+1 tablespoon tomato paste
+1-2 medium tomatoes
+Salt, pepper, sugar, other spices to taste really<br></br><br></br>
+
+The recipe itself is exactly as unspectacular as it sounds in the intro, but boy does look and taste spectacular! To begin, cut your zucchini and tofu into cubes, and your peppers and onion into strips. Start your olive oil on medium heat, then, when it’s hot, dump in all your veggies together with the tomato paste. Stir well, so the tomato paste gets incorporated, then add your spices. I used salt, ground black pepper, a little bit of sugar, cayenne pepper, ground cumin and a little bit of turmeric, to get a slightly spicy, Indian inspired flavor. Stir well and cook on medium heat for 7-10- mins, then cover and cook on low heat for 10 mins, or until your vegetables are soft. Your tomatoes will probably nearly fully dissolve, but that’s the point, because they add to the sauce. You can simply stir in your tofu according to its cooking time, whatever kind you’re using. I had smoked tofu on hand, so I just put it in 5 mins before everything was done, for a nice smoky addition to the veggies. The dish goes very well with a non-intrusive, fresh salad, the one in the photo is just tomato, cucumber, salt, black pepper, olive oil and vinegar-simple as that!
+    </p>,
+  },
+  {
+    name: "Pea Butter Gnocchi",
+    image: Gnocchi,
+    CookTime: 25,
+    recipe: <p>
+      Never in my life would i think to combine peas and gnocchi, but hey, if @gordongram says it works, then it works. And boy does it work!<br></br><br></br>
+
+INGREDIENTS:<br></br><br></br>
+1 pack of your favorite gnocchi<br></br>
+150-300g peas<br></br>
+Butter<br></br>
+Salt & black pepper<br></br>
+Olive oil<br></br>
+The zest of 1-2 lemons<br></br>
+Grated cheese<br></br><br></br>
+
+Now I’m no good at making gnocchi myself, nor do i have the time to do that, so i just took my favorite ones from the supermarket, though i find that seashell shapes work best. To start, you need to set a pot of water to a boil. When the water’s a-bubblin, add your gnocchi and cook until they start to float – in most cases only a couple of minutes. Drain the gnocchi and set aside. For the “sauce” you’ll need the rest of the stuff, make sure your peas are defrosted if using frozen ones, we’ll only have them in the pan for a short while. Add some olive oil to a medium-high heat pan, then add your gnocchi. Basically what we want here is for the gnocchi to get some color on both sides, this will change the texture as well. This should take 1-2 minutes per side as well. When your gnocchi have attained some color, add the peas in to heat through, along with a knob of butter. Season with a pinch or two of salt and ground black pepper. Make sure everything is nicely mixed, then add the lemon zest – this is the most important part of the dish. You can add the zest of one or two lemons, I prefer two for added freshness. Finish with a grated cheese of your choice and there you have it – something new and interesting and it only took you 20 minutes!
+    </p>,
+  },
+  {
+    name: "Honey Chicken Roast",
+    image: Pot,
+    CookTime: 50,
+    recipe: <p>
+      Roasted Chicken and vegetables in Honey Sauce<br></br><br></br>
+
+A sweet twist on an old classic,made simple and delicious by the old saying "stick some stuff in a pan until it turns tan" .For anyone tired of dry chicken and boring charred vegetables,this is a simple way to change up your saturday lunch.<br></br><br></br>
+
+Ingredients:<br></br><br></br>
+Meat:<br></br>
+
+-2 chicken wings (can be replaced by drumsticks or thigh meat)
+-1 tablespoon of salt
+-1 tablespoon of pepper
+-2 tablespoons of oregano
+1-tablespoon of sweet paprika<br></br><br></br>
+
+Vegetables:<br></br>
+-2 potatos
+-2 red bell peppers
+-250 g of mushrooms
+-1 red onion<br></br><br></br>
+
+Sauce:<br></br>
+-1 tablespoon of ketchup
+-2 tablespoons of honey
+-2 tablespoons of soy sauce<br></br><br></br>
+
+
+
+Instructions:<br></br><br></br>
+
+(optional):Make incisions on the side of your chicken pieces and then boil then until they turn a yellow color.Frying or baking chicken pieces with bones still in can make the skin a protective barrier making the heat unable to penetrate to the inner parts,leaving the chicken raw on the inside.
+This step will prevent that.<br></br><br></br>
+
+After you boiled your chicken,season it with salt,pepper,oregano and paprika.Place in in a pan covered in olive oil and sear until golden brown on both sides (1-2 min per side).This will only make the chicken crunchy and give it some color,not cook it through.Once done remove your chicken from the pan and set aside.<br></br><br></br>
+
+In a cup,mix ketchup ,soy sauce and honey and stir with a spoon until homogenous(Fancy way of saying stir until incorporated)<br></br><br></br>
+
+In the pan where you seared your chicken,add your diced vegetables(listed above) and sauttee for 2-3 minutes on mdium to low heat.Remove the pan from the stove and place your chicken atop of your vegetables.Cover with your sauce making sure everything is covered.Place your pan in a oven preheated to 180 C (360F) for 30 min,flipping the chicken halfway through.Once finished,let cool and enjoy!
+    </p>,
+  },
+  {
+    name: "Easy Porkloin with Veg",
+    image: Porkloin,
+    CookTime: 40,
+    recipe: <p>
+      Marinated pork loin and corn salsa<br></br><br></br>
+
+Something super simple for today! This one’s a honestly a very low effort-high reward kind of thing, quick, cheap and easy, the three best characteristics for a cool weeknight dinner!<br></br><br></br>
+MAIN INREDIENTS:<br></br><br></br>
+400g pork loin
+200-300g corn, raw kernels or canned is fine
+100-150g cherry tomatoes
+75g fresh spinach
+2 tablespoons olive oil
+Salt, pepper, salsa spices and acetto balsamico to taste<br></br><br></br>
+FOR THE MARINADE:<br></br><br></br>
+Mustard, ketchup, soy sauce, red pepper, garlic powder, salt, pepper, Worcestershire sauce, honey<br></br><br></br>
+
+There’s no need to be afraid of all these ingredients, yes there’s a lot of them, but you’re gonna be buying them to last for a while, and they all just mix together anyway into a really cool marinade. So start off by combining your marinade ingredients and dunking your pork in, dose the ingredients according to your own tastes, but consider that it tastes best when it’s all barbecue-y. Let your meat marinate for an hour at least, ideally overnight. When it comes time to cook it, just a couple of minutes each side will be enough for pork loin steaks to become nice and golden brown.<br></br><br></br>
+
+The salsa is done quite quickly as well, start off your corn in half a tablespoon of olive oil on medium heat. We’re using only a bit of oil, so the corn gets a nice char before we put the rest of the stuff in. Fun fact: we learned that from @gordongram himself, in fact it’s his 10 minute salsa recipe! So once your corn gets a nice char, dump your cherry tomatoes in just to get them to blister a bit, 1 minute away from finishing add you spinach and cook it just a little, it doesn’t need much anyway. Add the spices you prefer in a salsa, works best with a little cayenne pepper and dried chillis to give it a bit of a kick. Finish with the remaining olive oil and acetto to taste, you don’t want to miss this.<br></br><br></br>
+
+Et voila! Simple, cheap, easy, looks nice, done in 10 minutes, what more do you need?
+    </p>
+  },
+];
